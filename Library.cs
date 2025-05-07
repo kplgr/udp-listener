a82e836015ec96c37faca19727030910eae21a44
Konstantinos <s413855@LAP004716>
2025-05-07 12:52:27 +0100

    Refactoring

---
+++ Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpListener
+{
+    internal class Library
+    {
+        public enum FedLabsMessageValueTypes
+        {
+            Double = 8,
+            Single = 4,
+            Int32 = 4,
+            Int8 = 1
+        }
+
+        public record FedLabsMessagePart(FedLabsMessageValueTypes ValueType, string Name)
+        {
+            private FedLabsMessagePart() : this(default, string.Empty) { }
+
+            public static FedLabsMessagePart Double(string name)
+                => new(FedLabsMessageValueTypes.Double, name);
+
+            public static FedLabsMessagePart Single(string name)
+                => new(FedLabsMessageValueTypes.Single, name);
+
+            public static FedLabsMessagePart Int32(string name)
+                => new(FedLabsMessageValueTypes.Int32, name);
+
+            public static FedLabsMessagePart Int8(string name)
+                => new(FedLabsMessageValueTypes.Int8, name);
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,11 @@
 ﻿using System.Net.Sockets;
 using System.Net;
-using System.ComponentModel;
+using static UdpListener.Library;
 
 namespace UdpListener
 {
     internal class Program
     {
-        public enum FedLabsMessageValueTypes
-        {
-            Double = 8,
-            Single = 4,
-            Int32 = 4,
-            Int8 = 1 
-        }
-
-        public record FedLabsMessagePart(FedLabsMessageValueTypes ValueType, string Name)
-        {
-            private FedLabsMessagePart() : this(default, string.Empty) { }
-
-            public static FedLabsMessagePart Double(string name) 
-                => new(FedLabsMessageValueTypes.Double, name);
-
-            public static FedLabsMessagePart Single(string name)
-                => new(FedLabsMessageValueTypes.Single, name);
-
-            public static FedLabsMessagePart Int32(string name)
-                => new(FedLabsMessageValueTypes.Int32, name);
-
-            public static FedLabsMessagePart Int8(string name)
-                => new(FedLabsMessageValueTypes.Int8, name);
-        };
-
         static void Main(string[] args)
         {
             const int port = 5000;
@@ -53,105 +28,27 @@ namespace UdpListener
                 FedLabsMessagePart.Double("qy"),
                 FedLabsMessagePart.Double("qz"),
 
-
-
                 FedLabsMessagePart.Double("Wx [body]"),
                 FedLabsMessagePart.Double("Wy [body]"),
                 FedLabsMessagePart.Double("Wz [body]"),
-
             };
 
-            var messagePrototype = Array.Empty<FedLabsMessagePart>();
-
-
-
-
-
-
-
-            // Create a new UdpClient for listening on the given port
-            UdpClient udpListener = new UdpClient(port);
-
-            // Define the endpoint (localhost and port)
+            var udpListener = new UdpClient(port);
             var endPoint = new IPEndPoint(IPAddress.Any, port);
 
             Console.WriteLine($"Listening for UDP messages on port {port}...");
 
             ulong messageCount = 0;
-
-            // Continuously listen for incoming UDP packets
+            
             while (true)
             {
                 try
                 {
-                    // Receive the incoming UDP packet
                     byte[] receivedBytes = udpListener.Receive(ref endPoint);
+                    Console.WriteLine($"Message #{messageCount++}: {receivedBytes.Length} bytes from {endPoint.Address}:{endPoint.Port} at {DateTime.UtcNow}");
 
-                    // Convert the received bytes to a string
                     //string receivedMessage = Encoding.UTF8.GetString(receivedBytes);
-
-                    (int quotient, int remainder) = Math.DivRem(receivedBytes.Length, 8);
-
-                    Console.WriteLine($"Message {messageCount++}: {receivedBytes.Length} bytes ({quotient} / {remainder}) from {endPoint.Address}:{endPoint.Port} at {DateTime.UtcNow}");
-
-                    if (receivedBytes.Length != defaultMessagePrototype.Sum(x => (int)x.ValueType))
-                    {
-                    
-
-                        Console.WriteLine($"\tUNEXPECTED MESSAGE LENGTH --- IMPROVISING\n");
-
-                        
-                        if (remainder != 0)
-                        {
-                            Console.WriteLine("\tMESSAGE LENGTH NOT MULTIPLE OF 8 BYTES --- FUCKING OFF\n");
-                            continue;
-                        }
-
-                        messagePrototype = Enumerable.Range(0, quotient).Select(x => FedLabsMessagePart.Double($"VAL {x}")).ToArray();
-
-                    }
-                    else
-                    {
-                        messagePrototype = defaultMessagePrototype;
-                    }
-                    
-
-                    int messageByteOffset = 0;
-
-                    Console.WriteLine("\tPosition\tType\t\tLength\t\tName\t\t\tContent");
-
-                    for (int i = 0; i < messagePrototype.Length; i++)
-                    {
-                        var part = messagePrototype[i];
-
-                        int valueTypeBytes = (int)part.ValueType;
-                        string valueTypeName = part.ValueType.ToString();
-
-                        object value;
-
-                        switch (part.ValueType)
-                        {
-                            case FedLabsMessageValueTypes.Double:
-                                value = BitConverter.ToDouble(receivedBytes, messageByteOffset);
-                                messageByteOffset += (int)FedLabsMessageValueTypes.Double;
-                                break;
-
-                            case FedLabsMessageValueTypes.Int32:
-                                value = BitConverter.ToInt32(receivedBytes, messageByteOffset);
-                                messageByteOffset += (int)FedLabsMessageValueTypes.Int32;
-                                break;
-
-                            default:
-                                throw new Exception("Unsupported type");
-                        }
-
-                        Console.WriteLine($"\t{i}\t\t{valueTypeName}\t\t{valueTypeBytes}\t\t{part.Name,-12}\t\t{value,+12:+0.000000;-0.000000; 0.000000}");
-
-
-
-                    }
-
-                    Console.WriteLine();
+                    DecodeMessage(receivedBytes, defaultMessagePrototype);
 
                 }
                 catch (Exception ex)
@@ -159,6 +56,72 @@ namespace UdpListener
                     Console.WriteLine($"Error: {ex.Message}");
                 }
             }
+        }
+
+        static void DecodeMessage(byte[] receivedBytes, FedLabsMessagePart[] defaultMessagePrototype)
+        {
+            var messagePrototype = Array.Empty<FedLabsMessagePart>();
+
+            // Check if the received message length matches the expected length
+            // If not, try to improvise a message prototype based on the received bytes
+
+            (int quotient, int remainder) = Math.DivRem(receivedBytes.Length, 8);
+
+            
+            if (receivedBytes.Length != defaultMessagePrototype.Sum(x => (int)x.ValueType))
+            {
+                Console.WriteLine($"\tUNEXPECTED MESSAGE LENGTH --- IMPROVISING\n");
+
+                if (remainder != 0)
+                {
+                    Console.WriteLine("\tMESSAGE LENGTH NOT MULTIPLE OF 8 BYTES --- FUCKING OFF\n");
+                    return;
+                }
+
+                messagePrototype = Enumerable.Range(0, quotient).Select(x => FedLabsMessagePart.Double($"VAL {x}")).ToArray();
+            }
+            else
+            {
+                messagePrototype = defaultMessagePrototype;
+            }
+
+
+            // Decode the message
+
+            int messageByteOffset = 0;
+
+            Console.WriteLine("\tPosition\tType\t\tLength\t\tName\t\t\tContent");
+
+            for (int i = 0; i < messagePrototype.Length; i++)
+            {
+                var part = messagePrototype[i];
+
+                int valueTypeBytes = (int)part.ValueType;
+                string valueTypeName = part.ValueType.ToString();
+
+                object value;
+
+                // Decode the value based on its type
+                switch (part.ValueType)
+                {
+                    case FedLabsMessageValueTypes.Double:
+                        value = BitConverter.ToDouble(receivedBytes, messageByteOffset);
+                        messageByteOffset += (int)FedLabsMessageValueTypes.Double;
+                        break;
+
+                    case FedLabsMessageValueTypes.Int32:
+                        value = BitConverter.ToInt32(receivedBytes, messageByteOffset);
+                        messageByteOffset += (int)FedLabsMessageValueTypes.Int32;
+                        break;
+
+                    default:
+                        throw new Exception($"No converter registered for {part.ValueType.GetType().FullName}");
+                }
+
+                Console.WriteLine($"\t{i}\t\t{valueTypeName}\t\t{valueTypeBytes}\t\t{part.Name,-12}\t\t\t{value,+12:+0.000000;-0.000000; 0.000000}");
+            }
+
+            Console.WriteLine();
         }
     }
 }
